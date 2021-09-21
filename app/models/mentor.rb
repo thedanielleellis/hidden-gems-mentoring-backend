c80e8c89c77d504271846141bdd4d3650b1e1d77
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-21 12:17:30 -0500

    mentor model, migration, seeds

---
+++ app/models/mentor.rb
@@ -0,0 +1,4 @@
+class Mentor < ApplicationRecord
+    belongs_to :department, dependent: :destroy
+    validates :title, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_170714) do
+ActiveRecord::Schema.define(version: 2021_09_21_171340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mentors", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.string "about"
+    t.string "image_url"
+    t.integer "department_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
