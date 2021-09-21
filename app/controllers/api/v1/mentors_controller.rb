3e0938916fc07d15236a9d577252d6f42b20f9a1
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-21 12:28:03 -0500

    added routes and controllers

---
+++ app/controllers/api/v1/mentors_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::MentorsController < ApplicationController
+    before_action :set_mentor, only: [:show, :update, :destroy]
+  
+    # GET /mentors
+    def index
+      @mentors = Mentor.all
+      render json: MentorSerializer.new(@mentors)
+    end
+  
+    # GET /mentors/1
+    def show
+      render json: @mentor
+    end
+  
+    # POST /mentors
+    def create
+      @mentor = Mentor.new(mentor_params)
+  
+      if @mentor.save
+        render json: MentorSerializer.new(@mentor), status: :accepted
+      else
+        render json: @mentor.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /mentors/1
+    def update
+      if @mentor.update(mentor_params)
+        render json: @mentor
+      else
+        render json: @mentor.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /mentors/1
+    def destroy
+      mentor = Mentor.find(params[:id])
+      @mentor.destroy
+      render json: @mentor
+  
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_mentor
+        @mentor = Mentor.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def mentor_params
+        params.require(:mentor).permit(:name, :title, :about, :image_url, :department_id)
+      end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  namespace :api do
+    namespace :v1 do
+      resources :mentors, only: [:index, :show, :update, :create, :destroy]
+      resources :departments, only: [:index, :show, :update, :create, :destroy]
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
+  end
